8915717f3bf1cca0bd642091eb5559e0ae513076
WeakSider <ozdamar.huseyin@outlook.com>
2022-04-21 20:50:31 +0300

    App is created

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,49 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var secondsPassed = 0
+    var totalTime = 0
+    var timer = Timer()
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var player: AVAudioPlayer!
     
-
-
+    @IBOutlet weak var timerProgress: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        
+        timerProgress.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTimer), userInfo:nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            timerProgress.progress = Float(secondsPassed) / Float(totalTime)
+            print(Float(secondsPassed) / Float(totalTime))
+            
+        } else {
+            
+            titleLabel.text = "DONE!"
+            timer.invalidate()
+            playSound()
+            
+        }
+    }
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
